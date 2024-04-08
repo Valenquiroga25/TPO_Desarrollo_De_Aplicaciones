5e43622fec3b6434d045971d80169013a232ad8f
Valentin Quiroga <valenquiroga2017@gmail.com>
2024-04-08 18:31:21 -0300

    Cambios en modelo personal

--- Api/src/main/java/com/uade/api/models/PersonalModel.java
+++ Api/src/main/java/com/uade/api/models/PersonalModel.java
@@ -23,7 +23,7 @@ public class PersonalModel {
     private String apellido;
     private String documento;
     private String password;
-    private Sector sector;
+    private String sector;
     private int categoria;
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime fechaIngreso;
