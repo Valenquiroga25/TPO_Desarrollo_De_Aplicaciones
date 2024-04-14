d971102dc5cd71de6a90358a23ab989d2b4f4010
Valentin Quiroga <valenquiroga2017@gmail.com>
2024-04-14 17:45:38 -0300

    TaTeTi casi terminado

--- PracticaTaTeTi/TaTeTi/app/src/main/java/com/example/tateti/ControlJuego.java
+++ PracticaTaTeTi/TaTeTi/app/src/main/java/com/example/tateti/ControlJuego.java
@@ -1,6 +1,7 @@
 package com.example.tateti;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class ControlJuego {
     String[][] tablero = new String[3][3];
@@ -21,55 +22,74 @@ public class ControlJuego {
     }
 
     public boolean gano(){
-        for(int i=0; i < tablero.length; i++){
-            for(int j=0; j < tablero[0].length; j++) {
-                if (!Objects.equals(tablero[i][j], "-")) {
-                    if (Objects.equals(tablero[i], 0)) {
-                        if (Objects.equals(tablero[i][0], tablero[i][1]) && Objects.equals(tablero[i][0], tablero[i][2]) ||
-                                ((Objects.equals(tablero[i][0], tablero[1][1]) && Objects.equals(tablero[i][0], tablero[2][2])) ||
-                                        (Objects.equals(tablero[i][0], tablero[1][0]) && Objects.equals(tablero[i][0], tablero[2][0]))))
-                            return true;
-                    }
+        return VerificarHorizontal() || VerificarVertical() || VerificarDiagonal();
+    }
 
-                    if (Objects.equals(tablero[j], 1)) {
-                        if (Objects.equals(tablero[0][j], tablero[1][j]) && Objects.equals(tablero[0][j], tablero[2][j]))
-                            return true;
-
-                    }
-
-                    if (Objects.equals(tablero[j], 2)) {
-                        if (Objects.equals(tablero[0][j], tablero[1][j]) && Objects.equals(tablero[0][j], tablero[2][j]) ||
-                                (Objects.equals(tablero[0][j], tablero[1][1]) && Objects.equals(tablero[0][j], tablero[0][2])))
-                            return true;
-
-                    }
-
-                    if (Objects.equals(tablero[i], 1)) {
-                        if (Objects.equals(tablero[i][0], tablero[i][1]) && Objects.equals(tablero[i][0], tablero[i][2]))
-                            return true;
-                    }
-                    if (Objects.equals(tablero[i], 2)) {
-                        if (Objects.equals(tablero[i][0], tablero[i][1]) && Objects.equals(tablero[i][0], tablero[i][2]))
-                            return true;
-                    }
-                }
-            }
+    private boolean VerificarHorizontal(){
+        for(int i=0;i<3;i++){
+            if(!Objects.equals(tablero[i][0], "-") && !Objects.equals(tablero[i][1], "-") && !Objects.equals(tablero[i][2], "-"))
+                if(Objects.equals(tablero[i][0], tablero[i][1]) && Objects.equals(tablero[i][0], tablero[i][2]))
+                    return true;
         }
         return false;
     }
 
-    public void proximoMovimiento(String usa) {
-        int random1 = (int)(Math.random()*3+1);
-        int random2 = (int)(Math.random()*3+1);
+    private boolean VerificarVertical(){
+        for(int i=0;i<3;i++){
+            if(!Objects.equals(tablero[0][i], "-") && !Objects.equals(tablero[1][i], "-") && !Objects.equals(tablero[2][i], "-"))
+                if((Objects.equals(tablero[0][i], tablero[1][i])) && (Objects.equals(tablero[1][i], tablero[2][i])))
+                    return true;
+        }
+        return false;
+    }
+
+    private boolean VerificarDiagonal(){
+        if((!Objects.equals(tablero[0][0], "-") && !Objects.equals(tablero[1][1], "-") && !Objects.equals(tablero[2][2], "-") ||
+                (!Objects.equals(tablero[2][0], "-") && !Objects.equals(tablero[1][1], "-") && !Objects.equals(tablero[0][2], "-"))))
+            if((Objects.equals(tablero[0][0], tablero[1][1]) && Objects.equals(tablero[1][1], tablero[2][2])) ||
+                    (Objects.equals(tablero[2][0], tablero[1][1]) && Objects.equals(tablero[1][1], tablero[0][2])))
+                return true;
+        return false;
+    }
+
+
+    public int[] proximoMovimiento(String usa) {
+        Random ran = new Random();
+        int random1 = ran.nextInt(tablero.length);
+        int random2 = ran.nextInt(tablero.length);
 
         while(!Objects.equals(tablero[random1][random2], "-")){
-            random1 = (int)(Math.random()*3+1);
-            random2 = (int)(Math.random()*3+1);
+            random1 = ran.nextInt(tablero.length);
+            random2 = ran.nextInt(tablero.length);
         }
 
-        if (Objects.equals(usa, "X"))
+        int[] numeros = new int[2];
+        numeros[0] = random1;
+        numeros[1] = random2;
+
+        if (Objects.equals(usa, "cruces"))
             tablero[random1][random2] = "O";
         else
             tablero[random1][random2] = "X";
+
+        return numeros;
+    }
+
+    public void reiniciar(){
+        for(int i=0; i < tablero.length; i++){
+            for(int j=0; j < tablero[0].length; j++){
+                tablero[i][j] = "-";
+            }
+        }
+    }
+
+    public boolean terminoEmpate(){
+        for(int i = 0; i < tablero.length;i++){
+            for(int j=0;j<tablero[0].length;j++){
+                if(Objects.equals(tablero[i][j], "-"))
+                    return false;
+            }
+        }
+        return true;
     }
 }
--- PracticaTaTeTi/TaTeTi/app/src/main/java/com/example/tateti/GameActivity.java
+++ PracticaTaTeTi/TaTeTi/app/src/main/java/com/example/tateti/GameActivity.java
@@ -11,7 +11,7 @@ public class GameActivity extends AppCompatActivity {
     TextView lblJugador, lblGanador;
 
     Button btnCentro, btnCentroArriba, btnCentroAbajo, btnCentroIzquierda,
-            btnCentroDerecha, btnArribaDerecha, btnArribaIzquierda, btnAbajoIzquierda, btnAbajoDerecha;
+            btnCentroDerecha, btnArribaDerecha, btnArribaIzquierda, btnAbajoIzquierda, btnAbajoDerecha, Reiniciar;
 
     String nombre,usa;
 
@@ -41,55 +41,81 @@ public class GameActivity extends AppCompatActivity {
         btnCentroAbajo = (Button) findViewById(R.id.btnCentroAbajo);
         btnAbajoDerecha = (Button) findViewById(R.id.btnAbajoDerecha);
         lblGanador = (TextView) findViewById(R.id.lblGanador);
+        Reiniciar = (Button) findViewById(R.id.btnReiniciar);
+
+        Button[][] botones = new Button[3][3];
+        botones[0][0] = btnArribaIzquierda;
+        botones[0][1] = btnCentroArriba;
+        botones[0][2] = btnArribaDerecha;
+        botones[1][0] = btnCentroIzquierda;
+        botones[1][1] = btnCentro;
+        botones[1][2] = btnCentroDerecha;
+        botones[2][0] = btnAbajoIzquierda;
+        botones[2][1] = btnCentroAbajo;
+        botones[2][2] = btnAbajoDerecha;
 
         btnArribaIzquierda.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,0,0, botones, usa);}
         });
 
         btnCentroArriba.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,0,1, botones, usa);}
         });
 
         btnArribaDerecha.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,0,2, botones, usa);}
         });
 
         btnCentroIzquierda.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,1,0, botones, usa);}
         });
 
         btnCentro.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,1,1, botones, usa);}
         });
 
         btnCentroDerecha.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,1,2, botones, usa);}
         });
 
         btnAbajoIzquierda.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,2,0, botones, usa);}
         });
 
         btnCentroAbajo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,2,1, botones, usa);}
         });
 
         btnAbajoDerecha.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {controloBoton(v);}
+            public void onClick(View v) {controloBoton(v,2,2, botones, usa);}
+        });
+
+        Reiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=0;i < 3;i++){
+                    for(int j=0;j < 3;j++){
+                        botones[i][j].setText("-");
+                        botones[i][j].setEnabled(true);
+                        cj.reiniciar();
+                        lblGanador.setText("El ganador es...");
+                    }
+                }
+            }
         });
     }
 
     @SuppressLint("SetTextI18n")
-    private void controloBoton(View v){
+    private void controloBoton(View v, int i, int j, Button[][] botones, String usa){
         if(tengoTurno) {
             Button seleccionado = (Button) v;
             if (usa.equalsIgnoreCase("circulos"))
@@ -98,13 +124,25 @@ public class GameActivity extends AppCompatActivity {
                 seleccionado.setText("X");
             tengoTurno = false;
             seleccionado.setEnabled(false);// Deshabilito el boton para que no se elija mas de una vez.
-            cj.asignarValor(1,1,usa);
-            if(cj.gano())
+            cj.asignarValor(i,j,usa);
+            if(cj.gano()) {
                 lblGanador.setText("El ganador es " + nombre);
+            }else if(cj.terminoEmpate())
+                lblGanador.setText("El juego terminó en empate!");
             else {
-                cj.proximoMovimiento(usa);
+                int[] numeros = cj.proximoMovimiento(usa);
+                if(usa.equals("circulos")) {
+                    botones[numeros[0]][numeros[1]].setText("X");
+                    botones[numeros[0]][numeros[1]].setEnabled(false);
+                }
+                else {
+                    botones[numeros[0]][numeros[1]].setText("O");
+                    botones[numeros[0]][numeros[1]].setEnabled(false);
+                }
                 if(cj.gano())
                     lblGanador.setText("El ganador es la máquina");
+                else if(cj.terminoEmpate())
+                    lblGanador.setText("El juego terminó en empate!");
                 tengoTurno = true;
             }
         }
