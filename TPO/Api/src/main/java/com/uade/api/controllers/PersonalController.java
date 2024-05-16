527c81c3807861e6101667f847cd00436833ef80
elr0s <rosellomateo@gmail.com>
2024-05-16 09:47:04 -0300

    update controllers

--- TPO/Api/src/main/java/com/uade/api/controllers/PersonalController.java
+++ TPO/Api/src/main/java/com/uade/api/controllers/PersonalController.java
@@ -1,4 +1,22 @@
 package com.uade.api.controllers;
 
+import com.uade.api.services.PersonalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PersonalController {
+    @Autowired
+    private PersonalService personalService;
+    public ResponseEntity<?> getPersonalByLegajo(@PathVariable Long legajo){
+        try {
+            return new ResponseEntity<>(personalService.findPersonalByLegajo(legajo), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+    }
+
 }
