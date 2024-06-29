51957b6881e332007f06de1feabe0aa0b52708ad
Valentin Quiroga <valenquiroga2017@gmail.com>
2024-06-29 16:10:08 -0300

    Arreglado de imagenes en BackEnd

--- TPO/Api/src/main/java/com/uade/api/controllers/ImagenController.java
+++ TPO/Api/src/main/java/com/uade/api/controllers/ImagenController.java
@@ -1,9 +1,10 @@
 package com.uade.api.controllers;
 
+import com.uade.api.models.*;
 import com.uade.api.models.DTOs.ImagenDevueltaDTO;
-import com.uade.api.models.ImagenServicioModel;
-import com.uade.api.models.ServicioModel;
+import com.uade.api.services.DenunciasService;
 import com.uade.api.services.ImagenService;
+import com.uade.api.services.ReclamosService;
 import com.uade.api.services.ServicioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,8 +23,12 @@ public class ImagenController {
     private ImagenService imagenService;
     @Autowired
     private ServicioService servicioService;
+    @Autowired
+    private ReclamosService reclamosService;
+    @Autowired
+    private DenunciasService denunciasService;
 
-    @PostMapping("/servicio")
+    @PostMapping("/servicio/")
     public ResponseEntity<String> uploadImagenServicio(@RequestParam("archivo") MultipartFile archivo, @RequestParam("idServicio") Long idServicio) {
         try {
             ServicioModel servicio = this.servicioService.findServicioById(idServicio);
@@ -39,12 +44,12 @@ public class ImagenController {
     }
 
     @GetMapping("/servicio/{idServicio}")
-    public ResponseEntity<?> downloadAllServicio(@PathVariable Long idServicio) {
+    public ResponseEntity<?> downloadAllServicios(@PathVariable Long idServicio) {
         try{
             List<ImagenServicioModel> imagenes = this.imagenService.findImagenesByIdServicio(idServicio);
             List<ImagenDevueltaDTO> imagenesDevuetas = new ArrayList<>();
             for(ImagenServicioModel imagen : imagenes)
-                imagenesDevuetas.add(convertToDTO(imagen));
+                imagenesDevuetas.add(convertServicioToDTO(imagen));
 
             return new ResponseEntity<>(imagenesDevuetas, HttpStatus.OK);
         } catch (Exception e){
@@ -53,7 +58,7 @@ public class ImagenController {
     }
 
     @DeleteMapping("/servicio/{idServicio}")
-    public ResponseEntity<?> deleteAllServicio(@PathVariable Long idServicio) {
+    public ResponseEntity<?> deleteAllServicios(@PathVariable Long idServicio) {
         try{
             List<ImagenServicioModel> imagenes = this.imagenService.findImagenesByIdServicio(idServicio);
             for(ImagenServicioModel imagen : imagenes)
@@ -65,7 +70,99 @@ public class ImagenController {
         }
     }
 
-    private ImagenDevueltaDTO convertToDTO(ImagenServicioModel imagen){
+    @PostMapping("/reclamo/")
+    public ResponseEntity<String> uploadImagenReclamo(@RequestParam("archivo") MultipartFile archivo, @RequestParam("idReclamo") Long idReclamo) {
+        try {
+            ReclamoModel reclamo = this.reclamosService.findReclamoById(idReclamo);
+            ImagenReclamoModel imagenAux = new ImagenReclamoModel(archivo.getBytes(), reclamo);
+            imagenService.createImagenReclamo(imagenAux);
+            return ResponseEntity.ok("Imagen subida exitosamente.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al subir la imagen.");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/reclamo/{idReclamo}")
+    public ResponseEntity<?> downloadAllReclamos(@PathVariable Long idReclamo) {
+        try{
+            List<ImagenReclamoModel> imagenes = this.imagenService.findImagenesByIdReclamos(idReclamo);
+            List<ImagenDevueltaDTO> imagenesDevuetas = new ArrayList<>();
+            for(ImagenReclamoModel imagen : imagenes)
+                imagenesDevuetas.add(convertReclamoToDTO(imagen));
+
+            return new ResponseEntity<>(imagenesDevuetas, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+    }
+
+    @DeleteMapping("/reclamo/{idReclamo}")
+    public ResponseEntity<?> deleteAllReclamos(@PathVariable Long idReclamo) {
+        try{
+            List<ImagenReclamoModel> imagenes = this.imagenService.findImagenesByIdReclamos(idReclamo);
+            for(ImagenReclamoModel imagen : imagenes)
+                this.imagenService.deleteImagenReclamo(imagen.getIdImagen());
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("/denuncia/")
+    public ResponseEntity<String> uploadImagenDenuncia(@RequestParam("archivo") MultipartFile archivo, @RequestParam("idDenuncia") Long idDenuncia) {
+        try {
+            DenunciaModel denuncia = this.denunciasService.findDenunciaById(idDenuncia);
+            ImagenDenunciaModel imagenAux = new ImagenDenunciaModel(archivo.getBytes(), denuncia);
+            imagenService.createImagenDenuncia(imagenAux);
+            return ResponseEntity.ok("Imagen subida exitosamente.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al subir la imagen.");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/denuncia/{idDenuncia}")
+    public ResponseEntity<?> downloadAllDenuncias(@PathVariable Long idDenuncia) {
+        try{
+            List<ImagenDenunciaModel> imagenes = this.imagenService.findImagenesByIdDenuncias(idDenuncia);
+            List<ImagenDevueltaDTO> imagenesDevuetas = new ArrayList<>();
+            for(ImagenDenunciaModel imagen : imagenes)
+                imagenesDevuetas.add(convertDenunciaToDTO(imagen));
+
+            return new ResponseEntity<>(imagenesDevuetas, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+    }
+
+    @DeleteMapping("/denuncia/{idDenuncia}")
+    public ResponseEntity<?> deleteAllDenuncias(@PathVariable Long idDenuncia) {
+        try{
+            List<ImagenDenunciaModel> imagenes = this.imagenService.findImagenesByIdDenuncias(idDenuncia);
+            for(ImagenDenunciaModel imagen : imagenes)
+                this.imagenService.deleteImagenDeuncia(imagen.getIdImagen());
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+        }
+    }
+
+    private ImagenDevueltaDTO convertServicioToDTO(ImagenServicioModel imagen){
+        return new ImagenDevueltaDTO(imagen.getDatosImagenBase64());
+    }
+
+    private ImagenDevueltaDTO convertReclamoToDTO(ImagenReclamoModel imagen){
+        return new ImagenDevueltaDTO(imagen.getDatosImagenBase64());
+    }
+
+    private ImagenDevueltaDTO convertDenunciaToDTO(ImagenDenunciaModel imagen){
         return new ImagenDevueltaDTO(imagen.getDatosImagenBase64());
     }
 }
